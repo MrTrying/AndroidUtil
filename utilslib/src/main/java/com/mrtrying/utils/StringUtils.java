99a004693ea9ad9c8e33119b044bd80177d12ffa
mrtrying <30665145@qq.com>
2019-05-17 16:15:24 +0800

    Initial commit

---
+++ utilslib/src/main/java/com/mrtrying/utils/StringUtils.java
@@ -0,0 +1,10 @@
+package com.mrtrying.utils;
+
+/**
+ * Description : utils for string.
+ * PackageName : com.mrtrying.utils
+ * Created by mrtrying on 2019/5/17 16:07.
+ * e_mail : ztanzeyu@gmail.com
+ */
+public class StringUtils {
+}
